4dee2524a97c6ba20b2a2963b1e2c88bedce5df4
Juan Mojica <mojicajuan1104@gmail.com>
2023-02-20 21:21:07 -0500

    Programa final Fibonacci

---
+++ Principal.java
@@ -0,0 +1,9 @@
+public class Principal{
+    public static void main (String[] args){
+        Fibonacci calculo = new Fibonacci(3);
+        calculo.getFibonacci();
+        System.out.println(calculo.getFibonacci());
+        //Factorial fact = new Factorial(10);
+        //fact.getFactorial();
+    }
+}
